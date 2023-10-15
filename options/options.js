5f7169b240a8426eb12b461a3b702bb494929116
Crowdbotics <team@crowdbotics.com>
2023-10-15 23:57:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "misty-waterfall-43958",
+  url: "https://misty-waterfall-43958.botics.co",
+  api: "https://misty-waterfall-43958.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
